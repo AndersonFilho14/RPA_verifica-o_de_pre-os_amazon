799dae13aefb24f647f2a72ac8e0f85f5ed5577b
Anderson Filho <anderson.filho@parvi.local>
2024-07-11 08:57:56 -0300

    Faltando tirar a chance de erro de alguns casos

--- amazom.py
+++ amazom.py
@@ -14,17 +14,16 @@ browser = webdriver.Chrome(service=Service,options=options)
 
 browser.get('https://www.amazon.com.br/?&tag=hydrbrabk-20&ref=pd_sl_7rwd1q78df_e&adgrpid=155790195778&hvpone=&hvptwo=&hvadid=677606588104&hvpos=&hvnetw=g&hvrand=11779293125743238176&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001625&hvtargid=kwd-10573980&hydadcr=26346_11691057&gad_source=1')
 
-lista = ['rico','batata','bone','celular','headphone','motorola','capacete','Caneca Para Chopp Mass Krug','banana']
+lista = ['rico','bone','headphone','batata','celular','motorola','capacete','Caneca Para Chopp Mass Krug','banana']
 
 for i in lista:
     #Indo na aba de busca
     browser.find_element(By.ID,'twotabsearchtextbox').clear()
-    browser.find_element(By.ID,'twotabsearchtextbox').send_keys('batata',Keys.ENTER)
+    browser.find_element(By.ID,'twotabsearchtextbox').send_keys(i,Keys.ENTER)
     #Clicando na imagem 
     # WebDriverWait(browser,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'a-section a-spacing-base')))
     sleep(2)
-    
     browser.find_element(By.CLASS_NAME,'s-product-image-container').click()
     sleep(2)
-    browser.save_screenshot(f'batata.png')
+    browser.save_screenshot(f'{i}.png')
     
